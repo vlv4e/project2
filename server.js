e4ac3d542259f11f1fd6893425da5a0cbd400003
adelbh7 <adelbh7@gmail.com>
2025-01-15 19:03:21 +0300

    The initail setup

---
+++ server.js
@@ -0,0 +1,50 @@
+// =======================
+// 1. IMPORTS
+// =======================
+const express = require('express');
+const app = express();
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+
+
+
+// =======================
+// 2. MIDDLEWARE
+// =======================
+app.use(express.urlencoded({ extended: false })); // parses the request body. Needed for the req.body
+app.use(methodOverride("_method")); // Will change the methods for
+app.use(morgan("dev")); // Logs the requests in the terminal
+
+
+// =======================
+// 3. CONNECTION TO DATABASE
+// =======================
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{console.log("Connected to DATABSE")})
+.catch(()=>{console.log("ERROR CONNECTING TO DB OMAR")})
+
+
+
+
+// =======================
+// 4. ROUTES
+// =======================
+
+
+
+
+
+
+
+
+
+
+// =======================
+// 5. LISTENING ON PORT 3000
+// =======================
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
