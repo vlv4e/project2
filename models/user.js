07fac463cf9486f911ca6940c7161602df08a932
vlv4e <mhndbinbader@gmail.com>
2025-01-19 20:25:40 +0300

    creating models

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isEmployer:{
+    type:Boolean,
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+// usertype
