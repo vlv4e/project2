f776ab96e1e5ba44711a366b62ed79fe8cf1674e
walaamadan20 <walaamadan@outlook.com>
2025-01-21 20:13:23 +0300

    edited modules and application at routes

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 require('dotenv').config()
 const mongoose = require("mongoose")
+const session = require('express-session');
 
-console.log("Div Branch")
 
 
 
@@ -19,13 +19,37 @@ app.use(express.urlencoded({ extended: false })); // parses the request body. Ne
 app.use(methodOverride("_method")); // Will change the methods for
 app.use(morgan("dev")); // Logs the requests in the terminal
 
+const isSignedIn = require("./middleware/is-signed-in.js")
+const passUserToView = require('./middleware/pass-user-to-view.js')
+const applicationsController = require('./controllers/applications.js');
+const authController = require('./controllers/auth.js');
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passUserToView); // use new passUserToView middleware here
+const path=require("path")
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+// middleware/is-signed-in.js
 
 // =======================
 // 3. CONNECTION TO DATABASE
 // =======================
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{console.log("Connected to DATABSE")})
-.catch(()=>{console.log("ERROR CONNECTING TO DB OMAR")})
+.catch(()=>{console.log("ERROR CONNECTING TO DB jobs")})
 
 
 
@@ -33,6 +57,24 @@ mongoose.connect(process.env.MONGODB_URI)
 // =======================
 // 4. ROUTES
 // =======================
+
+app.get('/', (req, res) => {
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
+  }
+});
+
+
+app.use('/auth', authController);
+
+app.use(isSignedIn)
+
+app.use("/users/:userId/applications",applicationsController)
 
 
 
